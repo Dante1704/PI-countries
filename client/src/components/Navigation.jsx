afcee7df11b8250ccff27839b7a067a200043e87
Dante <dante.nkaddarian@gmail.com>
2023-02-03 15:21:32 -0300

    cambios de estilo para pantalla de 1440px en adelante

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -95,54 +95,58 @@ const Nav = ({setCurrentPage} ) => {
             </div >
             <div className="menu-filters">
                 <form >
-                    <fieldset className="filters">
-                        <legend><h2>FILTERS</h2></legend>
+                    <div className="filters">
+                        
 
-                        <h4>CHOOSE A CONTINENT:</h4>
-                        <div className="continent-options">
-                            <input type="radio" name="continent" id="Antarctica" value="Antarctica" onChange={changeHandlerForContinent} />
-                            <label htmlFor="Antarctica" className="f-z-large" checked> Antarctica</label>
-
-                            <input type="radio" name="continent" id="Africa" value="Africa" onChange={changeHandlerForContinent} />
-                            <label htmlFor="Africa" className="f-z-large"> Africa</label>
-
-                            <input type="radio" name="continent" id="Asia" value="Asia" onChange={changeHandlerForContinent} />
-                            <label htmlFor="Asia" className="f-z-large"> Asia</label>
-
-                            <input type="radio" name="continent" id="Europe" value="Europe" onChange={changeHandlerForContinent} />
-                            <label htmlFor="Europe" className="f-z-large"> Europe</label>
-
-                            <input type="radio" name="continent" id="South America" value="South America" onChange={changeHandlerForContinent} />
-                            <label htmlFor="South America" className="f-z-large"> South America</label>
-
-                            <input type="radio" name="continent" id="North America" value="North America" onChange={changeHandlerForContinent} />
-                            <label htmlFor="North America" className="f-z-large"> North America</label>
-
-                            <input type="radio" name="continent" id="Oceania" value="Oceania" onChange={changeHandlerForContinent} />
-                            <label htmlFor="Oceania" className="f-z-large"> Oceania</label>
-                        </div>
-
-                        <label htmlFor="activities"><h4>CHOOSE AN ACTIVITY:</h4></label>
                         <div>
-                            <select name="activity" id="activities" onChange={changeHandlerForActivity}>
-                                <option value="" >--Please choose an option--</option>
-                                {
-                                    activitiesFiltered?.map(
-                                        (activity, index) =>
-                                            <option value={activity} key={index}>{activity}</option>
-                                    )
-                                }
-                            </select>
-                        </div>
+                            <h4>CHOOSE A CONTINENT:</h4>
+                            <div className="continent-options">
+                                <input type="radio" name="continent" id="Antarctica" value="Antarctica" onChange={changeHandlerForContinent} />
+                                <label htmlFor="Antarctica" className="f-z-large" checked> Antarctica</label>
 
-                        <h4>CHOSE A TYPE OF SORTING:</h4>
-                        <div className="sorting-filter">
-                            <input type="button" value="A To Z" className="buttons" onClick={clickHandlerForOrder} />
-                            <input type="button" value="Z To A" className="buttons" onClick={clickHandlerForOrder} />
-                            <input type="button" value="By Population Asc" className="buttons" onClick={clickHandlerForOrder} />
-                            <input type="button" value="By Population Des" className="buttons" onClick={clickHandlerForOrder} />
+                                <input type="radio" name="continent" id="Africa" value="Africa" onChange={changeHandlerForContinent} />
+                                <label htmlFor="Africa" className="f-z-large"> Africa</label>
+
+                                <input type="radio" name="continent" id="Asia" value="Asia" onChange={changeHandlerForContinent} />
+                                <label htmlFor="Asia" className="f-z-large"> Asia</label>
+
+                                <input type="radio" name="continent" id="Europe" value="Europe" onChange={changeHandlerForContinent} />
+                                <label htmlFor="Europe" className="f-z-large"> Europe</label>
+
+                                <input type="radio" name="continent" id="South America" value="South America" onChange={changeHandlerForContinent} />
+                                <label htmlFor="South America" className="f-z-large"> South America</label>
+
+                                <input type="radio" name="continent" id="North America" value="North America" onChange={changeHandlerForContinent} />
+                                <label htmlFor="North America" className="f-z-large"> North America</label>
+
+                                <input type="radio" name="continent" id="Oceania" value="Oceania" onChange={changeHandlerForContinent} />
+                                <label htmlFor="Oceania" className="f-z-large"> Oceania</label>
+                            </div>
                         </div>
-                    </fieldset>
+                        <div>
+                            <label htmlFor="activities"><h4>CHOOSE AN ACTIVITY:</h4></label>
+                            <div>
+                                <select name="activity" id="activities" onChange={changeHandlerForActivity}>
+                                    <option value="" >--Please choose an option--</option>
+                                    {
+                                        activitiesFiltered?.map(
+                                            (activity, index) =>
+                                                <option value={activity} key={index}>{activity}</option>
+                                        )
+                                    }
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                            <h4>CHOSE A TYPE OF SORTING:</h4>
+                            <div className="sorting-filter">
+                                <input type="button" value="A To Z" className="buttons" onClick={clickHandlerForOrder} />
+                                <input type="button" value="Z To A" className="buttons" onClick={clickHandlerForOrder} />
+                                <input type="button" value="By Population Asc" className="buttons" onClick={clickHandlerForOrder} />
+                                <input type="button" value="By Population Des" className="buttons" onClick={clickHandlerForOrder} />
+                            </div>
+                        </div>
+                    </div>
                     <button type="reset" className="btn-clean buttons" onClick={clickCleanHandler}>Clean Filters</button>
                 </form>
             </div>
